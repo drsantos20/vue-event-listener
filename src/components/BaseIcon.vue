8e0a429f01dc86eef63ea9d6676093e0475925be
daniel <drsantos20@gmail.com>
2020-05-18 18:11:21 -0300

    Add global components

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="icon-wrapper">
+    <svg class="icon" width="width" height="height">
+      <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 11
+    },
+    height: {
+      type: [Number, String],
+      default: 22
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+.icon {
+  width: 24px;
+  height: 24px;
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  margin-right: 6px;
+}
+</style>
